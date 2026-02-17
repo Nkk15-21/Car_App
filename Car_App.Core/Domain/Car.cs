3b9e63d8c2edfb401a287fceeed4fe339b894499
Ваше имя < email>
2026-02-17 17:30:05 +0200

    CarApp_COre

---
+++ Car_App.Core/Domain/Car.cs
@@ -0,0 +1,16 @@
+﻿namespace CarApp.Core.Domain
+{
+    public class Car
+    {
+        public Guid? Id { get; set; }
+
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
+        public int? Year { get; set; }
+        public string? Color { get; set; }
+        public int? Mileage { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+    }
+}
---
+++ Car_App.Core/ServiceInterface/ICarService.cs
@@ -0,0 +1,13 @@
+﻿using CarApp.Core.Dto;
+
+namespace CarApp.Core.ServiceInterface
+{
+    public interface ICarService
+    {
+        Task<CarDto?> GetByIdAsync(Guid id);
+        Task<IEnumerable<CarDto>> GetAllAsync();
+        Task<CarDto> CreateAsync(CarDto dto);
+        Task<CarDto> UpdateAsync(CarDto dto);
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
